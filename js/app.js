a2e95ac74ae86388aca857ba0f11dd2bb69ec919
Cenhelm Aston <55622895+Tell396@users.noreply.github.com>
2022-01-22 15:38:44 +1000

    Story #1. Added main files. Added smooth scroll.

---
+++ js/app.js
@@ -0,0 +1,11 @@
+import * as flsFunctions from './modules/functions.js'
+
+/*flsFunctions.isWebp()
+import Swiper, { Navigation, Pagination } from 'swiper'
+const swiper = new Swiper() */
+
+console.clear()
+console.dir('All is work :)')
+
+
+
